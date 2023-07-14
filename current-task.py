f1e55b337f7fd1f786a0394322ea9ad68c8c9f39
Chavanvit24 <71426772+Chavanvit24@users.noreply.github.com>
2023-07-14 08:56:06 +0700

    Update current-task.py

--- current-task.py
+++ current-task.py
@@ -13,3 +13,6 @@ async def main():
 
 # start the asyncio program
 asyncio.run(main())
+
+Fri Jul 14 08:48:10 2023 main coroutine started
+Fri Jul 14 08:48:10 2023<Task pending name='Task-1' coro=<main() running at c:\Asyncio-1\current-task.py:12> cb=[_run_until_complete_cb() at C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.11_3.11.1264.0_x64__qbz5n2kfra8p0\Lib\asyncio\base_events.py:180]>
